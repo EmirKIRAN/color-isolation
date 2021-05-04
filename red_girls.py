4a256696a160244a6705fdb7f2204e42699ee455
EmirKIRAN <emirr.kirann@gmail.com>
2021-05-04 19:53:49 +0300

    update the color filter

--- red_girls.py
+++ red_girls.py
@@ -4,18 +4,18 @@ from cv2 import cv2
 from skimage.io import imshow, imread,show
 from skimage.color import rgb2hsv, hsv2rgb
 
+# image read and write
 image = imread('image.png')
 plt.figure(num=None, figsize=(8, 6), dpi=80)
 imshow(image)
 
-red_filter_girl = (image[:,:,0]>150)
 
-red_girl_new = image.copy()
-red_girl_new[:, :, 0] = red_girl_new[:, :, 0]*red_filter_girl
-red_girl_new[:, :, 1] = red_girl_new[:, :, 1]*red_filter_girl
-red_girl_new[:, :, 2] = red_girl_new[:, :, 2]*red_filter_girl
-
+# we can clearly see that the red girl is isolated from the rest of the image. 
+red_filtered = (image[:,:,0] > 150) & (image[:,:,1] < 100) & (image[:,:,2] < 110)
 plt.figure(num=None, figsize=(8, 6), dpi=80)
+red_girl_new = image.copy()
+red_girl_new[:, :, 0] = red_girl_new[:, :, 0] * red_filtered
+red_girl_new[:, :, 1] = red_girl_new[:, :, 1] * red_filtered
+red_girl_new[:, :, 2] = red_girl_new[:, :, 2] * red_filtered
 imshow(red_girl_new)
-
 show()
